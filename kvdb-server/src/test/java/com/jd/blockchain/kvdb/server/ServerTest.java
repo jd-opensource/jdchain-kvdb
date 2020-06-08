4639729c4e56de3a08e9981862c1788465ceb7b7
huanghaiquan <huanghaiquan@jd.com>
2020-06-08 11:29:07 +0800

    fixed bugs of kvdb testcase "ServerTest";

--- kvdb-server/src/test/java/com/jd/blockchain/kvdb/server/ServerTest.java
+++ kvdb-server/src/test/java/com/jd/blockchain/kvdb/server/ServerTest.java
@@ -19,67 +19,77 @@ import java.util.concurrent.CountDownLatch;
 
 public class ServerTest {
 
-    @Before
-    public void setUp() {
-        System.setProperty("logging.path", "logs");
-    }
+	@Before
+	public void setUp() {
+		System.setProperty("logging.path", "logs");
+	}
 
-    @Test
-    public void testSingle() throws Exception {
-        KVDBServerContext context = new KVDBServerContext(new ServerConfig(this.getClass().getResource("/server/single").getFile()));
-        KVDBServer server = new KVDBServer(context);
-        server.start();
+	@Test
+	public void testSingle() throws Exception {
+		KVDBServerContext context = new KVDBServerContext(
+				new ServerConfig(this.getClass().getResource("/server/single").getFile()));
+		KVDBServer server = new KVDBServer(context);
+		server.start();
 
-        CountDownLatch reqCdl = new CountDownLatch(1);
-        KVDBConfig config = context.getConfig().getKvdbConfig();
-        NettyClient client = new NettyClient(new ClientConfig("localhost", config.getPort(), ""), () -> reqCdl.countDown());
-        reqCdl.await();
-        Response response = client.send(KVDBMessage.use("test1"));
-        Assert.assertEquals(Constants.SUCCESS, response.getCode());
-        DatabaseClusterInfo clusterInfo = BinaryProtocol.decodeAs(response.getResult()[0].toBytes(), DatabaseClusterInfo.class);
-        Assert.assertFalse(clusterInfo.isClusterMode());
+		CountDownLatch reqCdl = new CountDownLatch(1);
+		KVDBConfig config = context.getConfig().getKvdbConfig();
+		NettyClient client = new NettyClient(new ClientConfig("localhost", config.getPort(), ""),
+				() -> reqCdl.countDown());
+		reqCdl.await();
+		Response response = client.send(KVDBMessage.use("test1"));
+		Assert.assertEquals(Constants.SUCCESS, response.getCode());
+		DatabaseClusterInfo clusterInfo = BinaryProtocol.decodeAs(response.getResult()[0].toBytes(),
+				DatabaseClusterInfo.class);
+		Assert.assertFalse(clusterInfo.isClusterMode());
 
-        server.stop();
+		server.stop();
 
-        FileUtils.deletePath(new File(context.getConfig().getKvdbConfig().getDbsRootdir()), true);
-    }
+		FileUtils.deletePath(new File(context.getConfig().getKvdbConfig().getDbsRootdir()), true);
+	}
 
-    @Test
-    public void testCluster() throws Exception {
-        KVDBServerContext context1 = new KVDBServerContext(new ServerConfig(this.getClass().getResource("/server/cluster/1").getFile()));
-        KVDBServer server1 = new KVDBServer(context1);
-        CountDownLatch cdl = new CountDownLatch(2);
-        new Thread(() -> {
-            server1.start();
-            cdl.countDown();
+	@Test
+	public void testCluster() throws Exception {
+		KVDBServerContext context1 = new KVDBServerContext(
+				new ServerConfig(this.getClass().getResource("/server/cluster/1").getFile()));
+		// clean dirs;
+		FileUtils.deletePath(new File(context1.getConfig().getKvdbConfig().getDbsRootdir()), true);
+		KVDBServer server1 = new KVDBServer(context1);
+		CountDownLatch cdl = new CountDownLatch(2);
+		new Thread(() -> {
+			server1.start();
+			cdl.countDown();
 
-        }).start();
+		}).start();
 
-        KVDBServerContext context2 = new KVDBServerContext(new ServerConfig(this.getClass().getResource("/server/cluster/2").getFile()));
-        KVDBServer server2 = new KVDBServer(context2);
-        new Thread(() -> {
-            server2.start();
-            cdl.countDown();
-        }).start();
+		KVDBServerContext context2 = new KVDBServerContext(
+				new ServerConfig(this.getClass().getResource("/server/cluster/2").getFile()));
+		// clean dirs;
+		FileUtils.deletePath(new File(context2.getConfig().getKvdbConfig().getDbsRootdir()), true);
+		KVDBServer server2 = new KVDBServer(context2);
+		new Thread(() -> {
+			server2.start();
+			cdl.countDown();
+		}).start();
 
-        cdl.await();
+		cdl.await();
 
-        CountDownLatch reqCdl = new CountDownLatch(1);
-        KVDBConfig config = context1.getConfig().getKvdbConfig();
-        NettyClient client = new NettyClient(new ClientConfig("localhost", config.getPort(), ""), () -> reqCdl.countDown());
-        reqCdl.await();
-        Response response = client.send(KVDBMessage.use("test1"));
-        Assert.assertEquals(Constants.SUCCESS, response.getCode());
-        DatabaseClusterInfo clusterInfo = BinaryProtocol.decodeAs(response.getResult()[0].toBytes(), DatabaseClusterInfo.class);
-        Assert.assertTrue(clusterInfo.isClusterMode());
-        Assert.assertEquals("test1", clusterInfo.getClusterItem().getName());
-        Assert.assertEquals(2, clusterInfo.getClusterItem().getURLs().length);
+		CountDownLatch reqCdl = new CountDownLatch(1);
+		KVDBConfig config = context1.getConfig().getKvdbConfig();
+		NettyClient client = new NettyClient(new ClientConfig("localhost", config.getPort(), ""),
+				() -> reqCdl.countDown());
+		reqCdl.await();
+		Response response = client.send(KVDBMessage.use("test1"));
+		Assert.assertEquals(Constants.SUCCESS, response.getCode());
+		DatabaseClusterInfo clusterInfo = BinaryProtocol.decodeAs(response.getResult()[0].toBytes(),
+				DatabaseClusterInfo.class);
+		Assert.assertTrue(clusterInfo.isClusterMode());
+		Assert.assertEquals("test1", clusterInfo.getClusterItem().getName());
+		Assert.assertEquals(2, clusterInfo.getClusterItem().getURLs().length);
 
+		server1.stop();
+		server2.stop();
 
-        server1.stop();
-        server2.stop();
-
-        FileUtils.deletePath(new File(context1.getConfig().getKvdbConfig().getDbsRootdir()), true);
-        FileUtils.deletePath(new File(context2.getConfig().getKvdbConfig().getDbsRootdir()), true);
-    }
+		FileUtils.deletePath(new File(context1.getConfig().getKvdbConfig().getDbsRootdir()), true);
+		FileUtils.deletePath(new File(context2.getConfig().getKvdbConfig().getDbsRootdir()), true);
+	}
 }

acdd74e826a790e1bd35b32099a4315ca2ab4502
imuge <imuge@qinest.com>
2020-09-01 17:41:58 +0800

    update benchmark

---
+++ kvdb-benchmark/src/main/java/com/jd/blockchain/kvdb/benchmark/BenchmarkRunner.java
@@ -0,0 +1,75 @@
+package com.jd.blockchain.kvdb.benchmark;
+
+import com.jd.blockchain.kvdb.client.KVDBClient;
+import com.jd.blockchain.kvdb.protocol.client.ClientConfig;
+import com.jd.blockchain.kvdb.protocol.exception.KVDBException;
+import com.jd.blockchain.utils.Bytes;
+import com.jd.blockchain.utils.io.BytesUtils;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+public class BenchmarkRunner implements Runnable {
+
+    private KVDBBenchmark bm;
+    private KVDBClient client;
+    private CountDownLatch cdl;
+
+    public BenchmarkRunner(ClientConfig config, KVDBBenchmark bm, CountDownLatch cdl) throws KVDBException {
+        this.bm = bm;
+        this.cdl = cdl;
+        this.client = new KVDBClient(config);
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (bm.batch && bm.keepAlive) {
+                client.batchBegin();
+            }
+            long i = 0;
+            long j = 0;
+            while (i < bm.requests) {
+                i++;
+                try {
+                    Bytes kv = randomBytes(bm.kvDataSize);
+                    if (bm.batch && bm.keepAlive) {
+                        client.put(kv, kv, true);
+                    } else {
+                        client.put(kv, kv);
+                    }
+                } catch (KVDBException e) {
+                    e.printStackTrace();
+                }
+                j++;
+                if (bm.batch && bm.keepAlive && bm.batchSize == j) {
+                    client.batchCommit();
+                    client.batchBegin();
+                    j = 0;
+                }
+            }
+            if (j > 0 && bm.batch && bm.keepAlive) {
+                client.batchCommit();
+            }
+
+            cdl.countDown();
+            client.close();
+        } catch (KVDBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final String STRING = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static Random random = new Random();
+
+    public static Bytes randomBytes(int size) {
+        if (size <= 0) {
+            return new Bytes(BytesUtils.EMPTY_BYTES);
+        }
+        StringBuilder sb = new StringBuilder(size);
+        for (int i = 0; i < size; i++)
+            sb.append(STRING.charAt(random.nextInt(STRING.length())));
+
+        return Bytes.fromString(sb.toString());
+    }
+}
--- kvdb-benchmark/src/main/java/com/jd/blockchain/kvdb/benchmark/KVDBBenchmark.java
+++ kvdb-benchmark/src/main/java/com/jd/blockchain/kvdb/benchmark/KVDBBenchmark.java
@@ -1,14 +1,9 @@
 package com.jd.blockchain.kvdb.benchmark;
 
-import com.jd.blockchain.kvdb.client.KVDBClient;
 import com.jd.blockchain.kvdb.protocol.client.ClientConfig;
-import com.jd.blockchain.kvdb.protocol.exception.KVDBException;
 import com.jd.blockchain.utils.ArgumentSet;
-import com.jd.blockchain.utils.Bytes;
 
-import java.util.Random;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class KVDBBenchmark {
 
@@ -39,15 +34,15 @@ public class KVDBBenchmark {
     private static final boolean DEFAULT_KEEP_ALIVE = true;
     private static final int DEFAULT_KV_DATA_SIZE = 16;
 
-    private String host;
-    private int port;
-    private String db;
-    private int clients;
-    private int requests;
-    private boolean batch;
-    private int batchSize;
-    private int kvDataSize;
-    private boolean keepAlive;
+    public String host;
+    public int port;
+    public String db;
+    public int clients;
+    public int requests;
+    public boolean batch;
+    public int batchSize;
+    public int kvDataSize;
+    public boolean keepAlive;
 
     public KVDBBenchmark(String[] args) {
         ArgumentSet arguments = ArgumentSet.resolve(args, ArgumentSet.setting().prefix(HOST, PORT, CLIENTS, REQUESTS, KV_DATA_SIZE, KEEPALIVE, BATCH, BATCH_SIZE, DB));
@@ -113,79 +108,24 @@ public class KVDBBenchmark {
             KVDBBenchmark bm = new KVDBBenchmark(args);
             ClientConfig config = new ClientConfig(bm.host, bm.port, bm.db);
             config.setKeepAlive(bm.keepAlive);
-            AtomicLong requests = new AtomicLong(bm.requests);
-            AtomicLong failCount = new AtomicLong(0);
-            CountDownLatch startCdl = new CountDownLatch(1);
-            CountDownLatch endCdl = new CountDownLatch(bm.clients);
-            for (int i = 0; i < bm.clients; i++) {
-                final int index = i;
-                new Thread(() -> {
-                    try {
-                        KVDBClient client = new KVDBClient(config);
-                        if (bm.batch && bm.keepAlive) {
-                            client.batchBegin();
-                        }
-                        try {
-                            startCdl.await();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        long j = 0;
-                        while (requests.getAndDecrement() > 0) {
-                            try {
-                                Bytes kv = randomBytes(bm.kvDataSize);
-                                if (bm.batch && bm.keepAlive) {
-                                    client.put(kv, kv, true);
-                                } else {
-                                    client.put(kv, kv);
-                                }
-                            } catch (KVDBException e) {
-                                failCount.incrementAndGet();
-                                e.printStackTrace();
-                            }
-                            j++;
-                            if (bm.batch && bm.keepAlive && bm.batchSize == j) {
-                                client.batchCommit();
-                                client.batchBegin();
-                                j = 0;
-                            }
-                        }
-                        if (j > 0 && bm.batch && bm.keepAlive) {
-                            client.batchCommit();
-                        }
-                        endCdl.countDown();
-                        client.close();
-                    } catch (KVDBException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
+            Thread[] runners = new Thread[bm.clients];
+            CountDownLatch cdl = new CountDownLatch(runners.length);
+            for (int i = 0; i < runners.length; i++) {
+                runners[i] = new Thread(new BenchmarkRunner(config, bm, cdl));
             }
-
             long startTime = System.currentTimeMillis();
-            startCdl.countDown();
-            try {
-                endCdl.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            for (int i = 0; i < runners.length; i++) {
+                runners[i].start();
             }
+            cdl.await();
             long endTime = System.currentTimeMillis();
-            String result = String.format("requests:%d, clients:%d, batch:%s, batch_size:%d, kv_data_size:%dbytes, times:%dms, errors:%d, tps:%f",
-                    bm.requests, bm.clients, bm.batch, bm.batchSize, bm.kvDataSize, endTime - startTime, failCount.get(), bm.requests / ((endTime - startTime) / 1000d));
+            String result = String.format("requests:%d, clients:%d, batch:%s, batch_size:%d, kv_data_size:%dbytes, times:%dms, tps:%f",
+                    bm.requests * bm.clients, bm.clients, bm.batch, bm.batchSize, bm.kvDataSize, endTime - startTime, bm.requests * bm.clients / ((endTime - startTime) / 1000d));
             System.out.println(result);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static final String STRING = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    private static Random random = new Random();
-
-    public static Bytes randomBytes(int size) {
-        StringBuilder sb = new StringBuilder(size);
-        for (int i = 0; i < size; i++)
-            sb.append(STRING.charAt(random.nextInt(STRING.length())));
-
-        return Bytes.fromString(sb.toString());
-    }
 
 }
